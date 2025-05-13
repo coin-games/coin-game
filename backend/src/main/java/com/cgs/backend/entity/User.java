f98f474af89b977bd8071f9e9283c302c0656c57
Jung-kr <jungwy98@gmail.com>
2025-05-13 16:41:51 +0900

    feat: User 엔티티 작성

---
+++ backend/src/main/java/com/cgs/backend/entity/User.java
@@ -0,0 +1,33 @@
+package com.cgs.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    private String id;
+    private String nickname;
+    private String email;
+    private String password;
+    private LocalDateTime registeredAt;
+
+    @Builder
+    public User(String nickname, String email, String password) {
+        this.id = UUID.randomUUID().toString();
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.registeredAt = LocalDateTime.now();
+    }
+}
