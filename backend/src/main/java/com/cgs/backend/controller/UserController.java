ff7a1e2d6fd82bf6b26bc368c5c36748cdf91889
Jung-kr <jungwy98@gmail.com>
2025-05-14 13:42:25 +0900

    feat: @valid 에러 관련

--- backend/src/main/java/com/cgs/backend/controller/UserController.java
+++ backend/src/main/java/com/cgs/backend/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.cgs.backend.controller;
 
+import com.cgs.backend.common.response.CustomResponse;
+import com.cgs.backend.common.response.ResponseCode;
 import com.cgs.backend.dto.user.TokenResponse;
 import com.cgs.backend.dto.user.UserLoginRequest;
 import com.cgs.backend.dto.user.UserSignUpRequest;
-import com.cgs.backend.entity.User;
 import com.cgs.backend.service.user.UserLoginService;
 import com.cgs.backend.service.user.UserSignUpService;
 import jakarta.validation.Valid;
@@ -20,14 +21,13 @@ public class UserController {
     private final UserLoginService userLoginService;
 
     @PostMapping("/signup")
-    public ResponseEntity<User> signUp(@RequestBody @Valid UserSignUpRequest request) {
+    public ResponseEntity<CustomResponse<Void>> signUp(@RequestBody @Valid UserSignUpRequest request) {
         userSignUpService.signUp(request);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(CustomResponse.success(ResponseCode.USER_CREATE_SUCCESS));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponse> login(@RequestBody @Valid UserLoginRequest request) {
-        TokenResponse token = userLoginService.login(request);
-        return ResponseEntity.ok(token);
+    public ResponseEntity<CustomResponse<TokenResponse>> login(@RequestBody @Valid UserLoginRequest request) {
+        return ResponseEntity.ok(CustomResponse.success(ResponseCode.USER_LOGIN_SUCCESS, userLoginService.login(request)));
     }
 }
