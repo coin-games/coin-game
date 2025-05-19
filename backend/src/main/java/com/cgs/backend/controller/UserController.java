8990cc52819b8b09f17c599582d51c56dd002e1d
Jung-kr <jungwy98@gmail.com>
2025-05-19 17:45:59 +0900

    feat: CustomResponseEntity 헬퍼 클래스 도입으로 HTTP 상태 코드 일관성 개선
    - ResponseCode 기반의 status 값을 HTTP 응답 코드로 반영할 수 있도록 CustomResponseEntity 클래스 추가
    - 컨트롤러에서 ResponseEntity.ok(...) 대신 ResponseEntity.status(...).body(...) 방식으로 변경

--- backend/src/main/java/com/cgs/backend/controller/UserController.java
+++ backend/src/main/java/com/cgs/backend/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cgs.backend.controller;
 
 import com.cgs.backend.common.response.CustomResponse;
+import com.cgs.backend.common.response.CustomResponseEntity;
 import com.cgs.backend.common.response.ResponseCode;
 import com.cgs.backend.dto.user.TokenResponse;
 import com.cgs.backend.dto.user.UserLoginRequest;
@@ -23,12 +24,12 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseEntity<CustomResponse<Void>> signUp(@RequestBody @Valid UserSignUpRequest request) {
         userSignUpService.signUp(request);
-        return ResponseEntity.ok(CustomResponse.success(ResponseCode.USER_CREATE_SUCCESS));
+        return CustomResponseEntity.success(ResponseCode.USER_CREATE_SUCCESS);
     }
 
     @PostMapping("/login")
     public ResponseEntity<CustomResponse<TokenResponse>> login(@RequestBody @Valid UserLoginRequest request) {
-        return ResponseEntity.ok(CustomResponse.success(ResponseCode.USER_LOGIN_SUCCESS, userLoginService.login(request)));
+        return CustomResponseEntity.success(ResponseCode.USER_LOGIN_SUCCESS, userLoginService.login(request));
     }
 
     @GetMapping("/test")
