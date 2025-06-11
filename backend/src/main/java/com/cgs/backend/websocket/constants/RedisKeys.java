e8b9a640d3b51e4b70c9ae2bbc2cbb88344d4a8e
Jung-kr <jungwy98@gmail.com>
2025-06-11 14:34:36 +0900

    refactor: reredis key 상수값 관리 클래스

---
+++ backend/src/main/java/com/cgs/backend/websocket/constants/RedisKeys.java
@@ -0,0 +1,7 @@
+package com.cgs.backend.websocket.constants;
+
+public final class RedisKeys {
+    public static final String GAME_READY_PREFIX = "game_ready:";
+
+    private RedisKeys() {}  //인스턴스화 방지
+}
